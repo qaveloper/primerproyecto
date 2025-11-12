90b18e57bdce93383f248e7b0dd4d548fd772197
qaveloper <jjcc16.89gmail.com>
2025-11-12 16:30:10 -0400

    solo archivo modified

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -11,7 +11,7 @@ export default class LoginPage{
     async fillLoginForm(username: string, password: string){
         await this.page.fill('#username', username);
         await this.page.fill('#password', password);
-        //await this.page.click('#submit');
+        await this.page.click('#submit');
     }
 
     async submit(){
